7080fbc2b2c70292f25a6153778963b7b94c4d5d
maowarisa <maowarisa123@gmail.com>
2023-10-28 09:42:20 +1100

    Pie chart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 import LineChart from './components/LineChart/LineChart'
+import PieChartWithCustomizedLabel from './components/PieChartWithCustomizedLabel/PieChartWithCustomizedLabel'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <NavBar></NavBar>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <PieChartWithCustomizedLabel></PieChartWithCustomizedLabel>
     </>
   )
 }
